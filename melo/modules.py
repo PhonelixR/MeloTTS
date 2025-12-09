98742d2a7c5bf7e9601a1f8cbbbb37bb76115e79
PhonelixR <contact.ruymanphone@gmail.com>
2025-12-09 19:11:04 +0000

    Update modules.py

--- melo/modules.py
+++ melo/modules.py
@@ -4,7 +4,19 @@ from torch import nn
 from torch.nn import functional as F
 
 from torch.nn import Conv1d
-from torch.nn.utils import weight_norm, remove_weight_norm
+
+# Importar la nueva API de weight_norm con compatibilidad hacia atrás
+try:
+    # PyTorch >= 2.0 (nueva API)
+    from torch.nn.utils.parametrizations import weight_norm
+    from torch.nn.utils.parametrize import remove_parametrizations
+    
+    def remove_weight_norm(module, name='weight'):
+        """Wrapper para la nueva API de remove_parametrizations"""
+        remove_parametrizations(module, name, leave_parametrized=False)
+except ImportError:
+    # PyTorch < 2.0 (API vieja)
+    from torch.nn.utils import weight_norm, remove_weight_norm
 
 from . import commons
 from .commons import init_weights, get_padding
@@ -157,7 +169,7 @@ class WN(torch.nn.Module):
             cond_layer = torch.nn.Conv1d(
                 gin_channels, 2 * hidden_channels * n_layers, 1
             )
-            self.cond_layer = torch.nn.utils.weight_norm(cond_layer, name="weight")
+            self.cond_layer = weight_norm(cond_layer, name="weight")
 
         for i in range(n_layers):
             dilation = dilation_rate**i
@@ -169,7 +181,7 @@ class WN(torch.nn.Module):
                 dilation=dilation,
                 padding=padding,
             )
-            in_layer = torch.nn.utils.weight_norm(in_layer, name="weight")
+            in_layer = weight_norm(in_layer, name="weight")
             self.in_layers.append(in_layer)
 
             # last one is not necessary
@@ -179,7 +191,7 @@ class WN(torch.nn.Module):
                 res_skip_channels = hidden_channels
 
             res_skip_layer = torch.nn.Conv1d(hidden_channels, res_skip_channels, 1)
-            res_skip_layer = torch.nn.utils.weight_norm(res_skip_layer, name="weight")
+            res_skip_layer = weight_norm(res_skip_layer, name="weight")
             self.res_skip_layers.append(res_skip_layer)
 
     def forward(self, x, x_mask, g=None, **kwargs):
@@ -211,11 +223,11 @@ class WN(torch.nn.Module):
 
     def remove_weight_norm(self):
         if self.gin_channels != 0:
-            torch.nn.utils.remove_weight_norm(self.cond_layer)
+            remove_weight_norm(self.cond_layer)
         for l in self.in_layers:
-            torch.nn.utils.remove_weight_norm(l)
+            remove_weight_norm(l)
         for l in self.res_skip_layers:
-            torch.nn.utils.remove_weight_norm(l)
+            remove_weight_norm(l)
 
 
 class ResBlock1(torch.nn.Module):
@@ -578,21 +590,4 @@ class TransformerCouplingLayer(nn.Module):
         else:
             x1 = (x1 - m) * torch.exp(-logs) * x_mask
             x = torch.cat([x0, x1], 1)
-            return x
-
-        x1, logabsdet = piecewise_rational_quadratic_transform(
-            x1,
-            unnormalized_widths,
-            unnormalized_heights,
-            unnormalized_derivatives,
-            inverse=reverse,
-            tails="linear",
-            tail_bound=self.tail_bound,
-        )
-
-        x = torch.cat([x0, x1], 1) * x_mask
-        logdet = torch.sum(logabsdet * x_mask, [1, 2])
-        if not reverse:
-            return x, logdet
-        else:
             return x
